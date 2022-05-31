ddc0f20ae63f6352c7b0bd7986470561c88f2e04
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-05-31 16:58:17 +0100

    feat: Added auto-resizing to the Plotapi iFrame

--- report/src/components/blocks/Plotapi.vue
+++ report/src/components/blocks/Plotapi.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
+/* eslint-disable no-useless-escape */
+import { computed, ComputedRef, onMounted } from "vue";
+import contentWindowJs from "iframe-resizer/js/iframeResizer.contentWindow.js?raw";
+import { v4 as uuid4 } from "uuid";
+import iframeResize from "iframe-resizer/js/iframeResizer";
+
 const p = defineProps<{ iframeContent: string; singleBlockEmbed: boolean }>();
-const IFRAME_HEIGHT = "35rem";
+const iframeId = `iframe_${uuid4()}`;
+
+// Unescape script tags when embedding
+const iframeDoc: ComputedRef<string> = computed(() => {
+    return p.iframeContent
+        .replace("<body>", `<body><script>${contentWindowJs}<\/script>`)
+});
+
+onMounted(() => {
+    iframeResize({ checkOrigin: false, warningTimeout: 10000 }, `#${iframeId}`);
+});
 </script>
 
 <template>
-    <div
-        :style="{ height: IFRAME_HEIGHT }"
-        :class="[
-            'flex justify-center w-full',
-            { 'h-iframe': singleBlockEmbed },
-        ]"
-        data-cy="block-iframe"
-    >
-        <iframe
-            :style="{ border: 'none !important' }"
-            :class="['w-full', { 'h-iframe': singleBlockEmbed }]"
-            :srcdoc="p.iframeContent"
-            sandbox="allow-scripts"
-        />
-    </div>
+    <iframe
+        :id="iframeId"
+        :style="{ border: 'none !important' }"
+        :class="['w-full', { 'h-iframe': singleBlockEmbed }]"
+        :srcdoc="iframeDoc"
+        sandbox="allow-scripts"
+    />
 </template>
